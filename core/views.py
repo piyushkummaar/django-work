50f87330148a447385ec5a45ed4c2041d4665140
piyushkummar <pshkmr007@gmail.com>
2022-08-05 17:04:30 +0530

    PostMethod

--- core/views.py
+++ core/views.py
@@ -8,10 +8,13 @@ def main(request):
     return HttpResponse("Hello There!!")
 
 class Home(View):
-    def get(self, request):
-        data = "hellor there"
-        title = "Hello There it nice you reached here!!"
-        return render(request,"home.html",context={"title":title,"get_data":data})
-        
-    def post(self, request):
-        pass
+    def __init__(self):
+        self.data = "hellor there"
+        self.title = "Hello There it nice you reached here!!"
+
+    def get(self, request,*args,**kwargs):
+        return render(request,"home.html",context={"title":self.title,"get_data":self.data})
+    def post(self, request,*args,**kwargs):
+        print(request.GET)
+        return render(request,"home.html",context={"title":self.title,"get_data":self.data})
+
